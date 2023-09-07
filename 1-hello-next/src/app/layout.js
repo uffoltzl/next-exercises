657c61a644e8724273f3fb3a809204ce25838d0d
uffoltzl <61827376+uffoltzl@users.noreply.github.com>
2023-09-07 22:16:47 +0200

    add correction

--- 1-hello-next/src/app/layout.js
+++ 1-hello-next/src/app/layout.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
+  const timestamp = new Date().toLocaleString();
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>{timestamp}</footer>
       </body>
     </html>
   );
