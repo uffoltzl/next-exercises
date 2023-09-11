a9c1c807000ffe410cba09220b8d291af25aa6b5
uffoltzl <l.ufz@hotmail.fr>
2023-09-11 09:16:03 +0200

    exo 4

---
+++ 4-bright/src/components/Snippet/Snippet.js
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+function Snippet({ children }) {
+  const [isSnippetShown, setIsSnippetShown] = React.useState(false);
+
+  if (isSnippetShown) {
+    return children;
+  }
+
+  return (
+    <div className="reveal">
+      <button onClick={() => setIsSnippetShown(true)}>Reveal Content</button>
+    </div>
+  );
+}
+
+export default Snippet;
