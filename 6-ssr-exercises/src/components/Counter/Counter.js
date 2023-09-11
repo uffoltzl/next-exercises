6900df99518bab4a860934a8e8768d15461b92e0
uffoltzl <l.ufz@hotmail.fr>
2023-09-11 13:50:53 +0200

    exo 6

--- 6-ssr-exercises/src/components/Counter/Counter.js
+++ 6-ssr-exercises/src/components/Counter/Counter.js
@@ -1,22 +1,25 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const savedValue = window.localStorage.getItem("saved-count");
+
+    if (savedValue === null) {
+      return;
+    }
+
+    setCount(Number(savedValue));
+  }, []);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("saved-count", count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
       Count: {count}
     </button>
   );
