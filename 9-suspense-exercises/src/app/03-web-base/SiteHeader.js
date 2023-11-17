e9e8873b906442f9d67cf3b501ce199e06f2479a
uffoltzl <l.ufz@hotmail.fr>
2023-11-17 15:34:49 +0100

    suspense exo 3

--- 9-suspense-exercises/src/app/03-web-base/SiteHeader.js
+++ 9-suspense-exercises/src/app/03-web-base/SiteHeader.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { getNavLinks } from '@/helpers/web-base-helpers';
+import { getNavLinks } from "@/helpers/web-base-helpers";
 
-async function SiteHeader() {
+function SiteHeader() {
+  return (
+    <header className="site-header">
+      <Link href="" className="logo">
+        WebBase
+      </Link>
+      <React.Suspense>
+        <NavLinks />
+      </React.Suspense>
+    </header>
+  );
+}
+
+export default SiteHeader;
+
+async function NavLinks() {
   let navLinks = await getNavLinks();
 
   // Only show the first 4 links in the header.
   navLinks = navLinks.slice(0, 4);
 
   return (
-    <header className="site-header">
-      <Link href="" className="logo">
-        WebBase
-      </Link>
-      <nav>
-        <ol className="header-nav-links">
-          {navLinks.map(
-            ({ slug, label, href, type }) => (
-              <li key={slug}>
-                <Link
-                  href={href}
-                  className={`header-nav-link ${type}`}
-                >
-                  {label}
-                </Link>
-              </li>
-            )
-          )}
-        </ol>
-      </nav>
-    </header>
+    <nav>
+      <ol className="header-nav-links">
+        {navLinks.map(({ slug, label, href, type }) => (
+          <li key={slug}>
+            <Link href={href} className={`header-nav-link ${type}`}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ol>
+    </nav>
   );
 }
-
-export default SiteHeader;
