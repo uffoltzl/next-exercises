a2d43740e59a33b249b3cdc0694543920967f46c
uffoltzl <l.ufz@hotmail.fr>
2023-11-17 11:09:04 +0100

    suspense exo 2

---
+++ 9-suspense-exercises/src/app/02-interview/Comments.js
@@ -0,0 +1,14 @@
+import React, { Suspense } from "react";
+
+import { getComments } from "@/helpers/interview-helpers";
+import Comment from "@/components/Comment";
+
+async function Comments() {
+  const comments = await getComments();
+
+  return comments.map((comment) => (
+    <Comment key={comment.id} comment={comment} />
+  ));
+}
+
+export default Comments;
